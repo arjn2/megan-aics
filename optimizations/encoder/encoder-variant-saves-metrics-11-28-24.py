d8876f0d67bdac68e3963a7a6c75e2e6e644d6f6
arjn <arjunal9000@gmail.com>
2024-11-28 22:05:50 +0530

    Update encoder-variant-saves-metrics-11-28-24.py

--- optimizations/encoder/encoder-variant-saves-metrics-11-28-24.py
+++ optimizations/encoder/encoder-variant-saves-metrics-11-28-24.py
@@ -339,7 +339,8 @@ class MalwareStyleGAN:
 
 def build_malware_detection_model(input_shape, num_classes):
     model = tf.keras.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        tf.keras.layers.Input(shape=input_shape),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
         tf.keras.layers.MaxPooling2D((2, 2)),
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
         tf.keras.layers.MaxPooling2D((2, 2)),
